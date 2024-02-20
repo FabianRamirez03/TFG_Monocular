162002c5fa47cebebc8a33829c603ef5350d49bb
fabian <ramirezfabian003@gmail.com>
2024-02-19 21:05:35 -0600

    Agregado el data loader para cargar los datos de las imagenes etiquetadas

--- util.py
+++ util.py
@@ -383,8 +383,10 @@ def cleaning_wrong_directories_pipeline():
 
 # cleaning_wrong_directories_pipeline()
 # create_csv()
-update_csv()
+# update_csv()
 # csv_mainteinence()
 # find_and_remove_empty_directories()
 #  remove_accents_and_rename_directories()
 # convert_videos_dir_to_frame()
+print_data_rows_counter()
+print_label_counts()
