f59cca3a59bba48dd4faca7f718c35125c4dfa37
fabian <ramirezfabian003@gmail.com>
2024-04-24 01:14:20 -0600

    Agregados los leds de estado

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, scrolledtext
 from datetime import datetime
+from PIL import Image, ImageTk
 
 # Logic
 
@@ -43,6 +44,44 @@ def save_logs():
             file.write(log_text)
             ConsolePrint(f"Logs guardados en {filename}")
 
+
+def open_and_resize_image():
+    global input_image_label
+    file_path = filedialog.askopenfilename(
+        title="Open Image",
+        filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")],
+    )
+    ConsolePrint("Image selected: " + file_path)
+    if file_path:
+        with Image.open(file_path) as img:
+            resized_img = img.resize((426, 240), Image.Resampling.LANCZOS)
+            tk_image = ImageTk.PhotoImage(resized_img)
+
+            # Actualizar la imagen en el label
+            input_image_label.config(image=tk_image)
+            input_image_label.image = tk_image  # Mantener una referencia
+
+
+def update_single_led(flag, label):
+    global led_off_PI, led_on_PI
+    if flag:
+        label.config(image=led_on_PI)
+    else:
+        label.config(image=led_off_PI)
+
+
+def update_leds():
+    global nublado, noche, soleado, lluvia, neblina
+    global nublado_image, noche_image, soleado_image, lluvia_image, neblina_image
+
+    update_single_led(nublado, nublado_image)
+    update_single_led(noche, noche_image)
+    update_single_led(soleado, soleado_image)
+    update_single_led(lluvia, lluvia_image)
+    update_single_led(neblina, neblina_image)
+
+
+# Globals
 
 title_Lato = ("Lato", 22)
 
@@ -108,13 +147,13 @@ default_label.place(x=497, y=6)
 no_image_path = "gui_images\\no_image.png"
 no_image_PI = tk.PhotoImage(file=no_image_path)
 
-input_image_label = tk.Label(input_frame, image=no_image_PI)
+input_image_label = tk.Label(input_frame, image=no_image_PI, width=426, height=240)
 input_image_label.place(x=43, y=43)
 
-default_image_label = tk.Label(botton_frame, image=no_image_PI)
+default_image_label = tk.Label(botton_frame, image=no_image_PI, width=426, height=240)
 default_image_label.place(x=43, y=43)
 
-processed_image_label = tk.Label(botton_frame, image=no_image_PI)
+processed_image_label = tk.Label(botton_frame, image=no_image_PI, width=426, height=240)
 processed_image_label.place(x=497, y=43)
 
 
@@ -125,7 +164,7 @@ Upload_bt_PI = tk.PhotoImage(file=Upload_bt_path)
 Upload_button = tk.Button(
     input_frame,
     image=Upload_bt_PI,
-    command=button_clicked,
+    command=open_and_resize_image,
     borderwidth=0,
     highlightthickness=0,
     padx=0,
@@ -193,6 +232,97 @@ console = scrolledtext.ScrolledText(
     console_frame, height=15, width=57, font=("Lato", 9), state=tk.DISABLED
 )
 console.place(x=11, y=53)
+
+
+# State leds images
+
+led_off_PI = ImageTk.PhotoImage(Image.open("gui_images\\led_off.png"))
+led_on_PI = ImageTk.PhotoImage(Image.open("gui_images\\led_on.png"))
+
+nublado = False
+noche = False
+soleado = False
+lluvia = False
+neblina = False
+
+nublado_image = tk.Label(input_frame)
+nublado_image.place(x=43, y=313)
+
+noche_image = tk.Label(input_frame)
+noche_image.place(x=139, y=313)
+
+soleado_image = tk.Label(input_frame)
+soleado_image.place(x=223, y=313)
+
+lluvia_image = tk.Label(input_frame)
+lluvia_image.place(x=316, y=313)
+
+neblina_image = tk.Label(input_frame)
+neblina_image.place(x=395, y=313)
+
+update_leds()
+
+# State leds labels
+
+nublado_label = tk.Label(
+    input_frame,
+    text="Nublado",
+    bg="white",
+    font=("Lato", 14),
+    borderwidth=0,
+    highlightthickness=0,
+    padx=0,
+    pady=0,
+)
+nublado_label.place(x=61, y=312)
+
+noche_label = tk.Label(
+    input_frame,
+    text="Noche",
+    bg="white",
+    font=("Lato", 14),
+    borderwidth=0,
+    highlightthickness=0,
+    padx=0,
+    pady=0,
+)
+noche_label.place(x=157, y=312)
+
+soleado_label = tk.Label(
+    input_frame,
+    text="Soleado",
+    bg="white",
+    font=("Lato", 14),
+    borderwidth=0,
+    highlightthickness=0,
+    padx=0,
+    pady=0,
+)
+soleado_label.place(x=242, y=312)
+
+lluvia_label = tk.Label(
+    input_frame,
+    text="Lluvia",
+    bg="white",
+    font=("Lato", 14),
+    borderwidth=0,
+    highlightthickness=0,
+    padx=0,
+    pady=0,
+)
+lluvia_label.place(x=334, y=312)
+
+neblina_label = tk.Label(
+    input_frame,
+    text="Neblina",
+    bg="white",
+    font=("Lato", 14),
+    borderwidth=0,
+    highlightthickness=0,
+    padx=0,
+    pady=0,
+)
+neblina_label.place(x=414, y=312)
 
 # Ejecuta la ventana principal
 root.mainloop()
